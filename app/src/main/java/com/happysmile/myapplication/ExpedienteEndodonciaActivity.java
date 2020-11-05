d51a4a2eaae766fabafd6f7c033c2c07607fe9da
Diego Davila <davila9923@gmail.com>
2020-11-04 19:00:38 -0600

    UI Doctor Finalizada

---
+++ app/src/main/java/com/happysmile/myapplication/ExpedienteEndodonciaActivity.java
@@ -0,0 +1,14 @@
+package com.happysmile.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpedienteEndodonciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expediente_endodoncia);
+    }
+}
